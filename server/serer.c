4d27165eedc77f54ebb34a4c14d5fe4837644630
justinzhong7789 <justinzhong7789@gmail.com>
2020-11-09 01:06:20 -0800

    Initial set up (include stuff)

---
+++ server/serer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <time.h>
+
+int main(int argc, char** argv){
+
+    return 0;
+}
